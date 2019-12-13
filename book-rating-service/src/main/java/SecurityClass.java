cd97346b0c678ef5ae98673a25c907eb896b8022
znatok0 <34071110+znatok0@users.noreply.github.com>
2019-12-13 17:18:04 +0600

    Add files via upload

---
+++ book-rating-service/src/main/java/SecurityClass.java
@@ -0,0 +1,6 @@
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityClass extends WebSecurityConfigurerAdapter {
+}
