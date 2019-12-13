33d836d19a98947d1eb3e90e0d6bb26b16aad857
znatok0 <34071110+znatok0@users.noreply.github.com>
2019-12-13 17:17:13 +0600

    Add files via upload

--- book-information-service/src/main/java/com/example/bookinformationservice/BookInformationServiceApplication.java
+++ book-information-service/src/main/java/com/example/bookinformationservice/BookInformationServiceApplication.java
@@ -3,6 +3,8 @@ package com.example.bookinformationservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,4 +14,8 @@ public class BookInformationServiceApplication {
 		SpringApplication.run(BookInformationServiceApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
--- book-information-service/src/main/java/com/example/bookinformationservice/resources/BookResource.java
+++ book-information-service/src/main/java/com/example/bookinformationservice/resources/BookResource.java
@@ -2,13 +2,22 @@ package com.example.bookinformationservice.resources;
 
 
 import com.example.bookinformationservice.models.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/book")
 public class BookResource {
+
+    @Value("${api.key}")
+    private String apiKey;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @RequestMapping("/{bookID}")
     public Book getBookInfo(@PathVariable("bookID") int bookID){
